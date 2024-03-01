bff3b990f7ae9ba40fc46f6b074ba303d624df03
Manoel Arcanjo <manoel199.333@gmail.com>
2024-03-01 08:37:03 -0300

    :lipstick: feat: Estilização CSS da NavBar

--- src/components/section/Navbar.jsx
+++ src/components/section/Navbar.jsx
@@ -1,7 +1,20 @@
+import styles from './../../styles/Navbar.module.css';
+import {FaInstagram, FaLinkedinIn, FaGithub} from 'react-icons/fa';
+
 function Navbar() {
     return (
-        <nav>
-            <h1>NavBar</h1>
+        <nav className={styles.navbar}>
+            <ul>
+                <li>Apresentação</li>
+                <li>Habilidades</li>
+                <li>Projetos</li>
+            </ul>
+
+            <ul>
+                <li><FaInstagram size={30}/></li>
+                <li><FaLinkedinIn size={30}/></li>
+                <li><FaGithub size={30}/></li>
+            </ul>
         </nav>
     )
 }
